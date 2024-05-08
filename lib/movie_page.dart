c1745005418beb4320278871885d0e99d5cadd2f
ekremm-discovery <129485041+ekremm-discovery@users.noreply.github.com>
2024-05-08 11:32:31 +0400

    cubit api test

---
+++ lib/movie_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:poc_cubit_api_test/movie_cubit.dart';
+import 'package:poc_cubit_api_test/movie_state.dart';
+
+class MoviesPage extends StatefulWidget {
+  const MoviesPage({super.key});
+
+  @override
+  _MoviesPageState createState() => _MoviesPageState();
+}
+
+class _MoviesPageState extends State<MoviesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Trending Movies'),
+      ),
+      body: BlocBuilder<MoviesCubit, MoviesState>(
+        builder: (context, state) {
+          if (state is LoadingState) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (state is ErrorState) {
+            return Center(
+              child: Icon(Icons.close),
+            );
+          } else if (state is LoadedState) {
+            final movies = state.movies;
+
+            return ListView.builder(
+              itemCount: movies.length,
+              itemBuilder: (context, index) => Card(
+                child: ListTile(
+                  title: Text(movies[index].title),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(movies[index].urlImage),
+                  ),
+                ),
+              ),
+            );
+          } else {
+            return Container();
+          }
+        },
+      ),
+    );
+  }
+}
